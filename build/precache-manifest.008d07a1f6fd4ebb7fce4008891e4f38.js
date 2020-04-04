0239deaf508df256030ed58009546164a7a514af
Biola <abiola2326@gmail.com>
2020-04-04 04:19:12 +0100

    Added production build

---
+++ build/precache-manifest.008d07a1f6fd4ebb7fce4008891e4f38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e74740ab6afa01f8f3ebe24e92033fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e3a58d6459196787b2c",
+    "url": "/static/css/main.792c4a44.chunk.css"
+  },
+  {
+    "revision": "35658a72390815255e11",
+    "url": "/static/js/2.66c1b036.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.66c1b036.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e3a58d6459196787b2c",
+    "url": "/static/js/main.b6492198.chunk.js"
+  },
+  {
+    "revision": "9d2b4f7262278411ffe7",
+    "url": "/static/js/runtime-main.4464e187.js"
+  }
+]);
---
+++ build/precache-manifest.af2f909025c9c7ee2f2307fb6bccb2c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dcaa64e84a35b7e57d5b5be8114d1db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "647176508e8853d1dc43",
+    "url": "/static/css/main.792c4a44.chunk.css"
+  },
+  {
+    "revision": "35658a72390815255e11",
+    "url": "/static/js/2.66c1b036.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.66c1b036.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "647176508e8853d1dc43",
+    "url": "/static/js/main.09323be7.chunk.js"
+  },
+  {
+    "revision": "9d2b4f7262278411ffe7",
+    "url": "/static/js/runtime-main.4464e187.js"
+  }
+]);

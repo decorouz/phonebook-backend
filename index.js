268f9e740c726598667ced53887e3698ffdeb13f
Biola <abiola2326@gmail.com>
2020-04-05 23:39:51 +0100

    p3.24

--- index.js
+++ index.js
@@ -19,29 +19,6 @@ app.use(
   )
 );
 
-let persons = [
-  {
-    name: 'Arto Hellas',
-    number: '040-123456',
-    id: 1
-  },
-  {
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-    id: 2
-  },
-  {
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-    id: 3
-  },
-  {
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-    id: 4
-  }
-];
-
 app.get('/', (req, res) => {
   res.send('<h1>Phone Book</h1>');
 });
@@ -59,14 +36,9 @@ app.get('/info', (req, res) => {
 });
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find(person => person.id === id);
-
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
+  Person.findById(req.params.id).then(person => {
+    res.json(person.toJSON());
+  });
 });
 
 app.delete('/api/persons/:id', (req, res) => {
@@ -92,22 +64,15 @@ app.post('/api/persons', (req, res) => {
     });
   }
 
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    date: new Date(),
-    id: generateID()
-  };
+    date: new Date()
+  });
 
-  if (persons.find(p => p.name === person.name)) {
-    return res.status(400).json({
-      error: 'name must be unique'
-    });
-  }
-
-  persons = persons.concat(person);
-
-  res.json(person);
+  person.save().then(savedPerson => {
+    res.json(savedPerson.toJSON());
+  });
 });
 
 const PORT = process.env.PORT || 3002;
